73f9fdfe3fc137eb553c00f88c789a81acf6c751
clumsysoap <14192369+clumsysoap@user.noreply.gitee.com>
2024-12-04 23:04:42 +0800

    环境更改不用合并

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import  createApp  from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-
-createApp(App).use(store).use(router).mount('#app')
+import { createApp } from 'vue';
+import App from './App.vue'; // 导入您的根组件
+import router from './router'; // 如果您使用 vue-router
+import store from './store'; // 如果您使用 Vuex
+ 
+// 创建 Vue 应用实例
+const app = createApp(App);
