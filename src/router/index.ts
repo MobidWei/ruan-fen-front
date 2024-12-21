30a8a914e85e4589f449e5a8ba4f408287aac610
MobidWei <2294848660@qq.com>
2024-12-21 09:21:00 +0800

    科研用户

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
+import ResearchUser from '../views/ResearchUser.vue'
+import AdvancedSearch from '../views/AdvancedSearch.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -8,15 +10,16 @@ const routes: Array<RouteRecordRaw> = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path:'/ResearchUser',
+    name: 'ResearchUser',
+    component: ResearchUser
+  },
+  {
+    path:'/AdvancedSearch',
+    name: 'AdvancedSearch',
+    component: AdvancedSearch
   }
 ]
-
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,28 @@
 <template>
-  <div>首页</div>
+  <div class="App">
+    <my-header></my-header>
+    <my-search></my-search>
+  </div>
 </template>
 
 <script>
+import { ref } from 'vue';
+import header from '@/component/header.vue';
+import searchArea from '@/component/searchArea.vue';
+
 export default {
-  name: 'Index'
+  components: {
+     'my-header': header,
+     'my-search': searchArea,
+  },
+  name: 'HomeIndex',
 }
 </script>
-<style scoped></style>
+
+<style scoped>
+.App {
+  width: 100%;
+  height: 100%;
+}
+</style>
 
